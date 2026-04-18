828c5aed7c6674ce13958159fb5a4410f7cda859
Ben Peachey <github@pother.ca>
2026-04-18 17:31:33 +0000

    Add dist files.

---
+++ dist/src/packages/tooltip/tooltip.d.ts
@@ -0,0 +1,20 @@
+import { ChildDom, State } from "../dom";
+import { TooltipPosition } from "./tooltipPosition";
+export declare const TooltipArrow: (props: {
+    tooltipPosition: State<TooltipPosition>;
+    tooltipBottomOverflow: State<boolean>;
+}) => HTMLDivElement;
+export type TooltipProps = {
+    position: TooltipPosition;
+    element: HTMLElement;
+    refreshes: State<number>;
+    hintMode: boolean;
+    showStepNumbers: boolean;
+    transitionDuration?: number;
+    autoPosition: boolean;
+    positionPrecedence: TooltipPosition[];
+    onClick?: (e: any) => void;
+    className?: string;
+    text: string;
+};
+export declare const Tooltip: ({ position: initialPosition, element, refreshes, hintMode, showStepNumbers, transitionDuration, positionPrecedence, className, autoPosition, text, onClick, }: TooltipProps, children?: ChildDom[]) => HTMLDivElement;
